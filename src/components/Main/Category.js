93b83266db7ae940cbf374c6a3b1cc851fc2b28c
lautaro porlan <l.porlann@gmail.com>
2021-04-11 02:18:01 -0300

    products import

--- src/components/Main/Category.js
+++ src/components/Main/Category.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
+
 const Category = () => {
     return (
         <div className="col-lg-6 mb-4">
             <div className="card bg-info text-white shadow">
-				<div className="card-body">Category 01</div>
+				<div className="card-body">categoria..</div>
             </div>
 		</div>
     );
