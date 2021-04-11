11ccf8aba431f235de4f008481272f1d3c74b36a
lautaro porlan <l.porlann@gmail.com>
2021-04-11 02:55:28 -0300

    category hook in progress

--- src/components/Main/Category.js
+++ src/components/Main/Category.js
@@ -1,11 +1,31 @@
-import React from 'react';
+import React , {useState , useEffect} from 'react';
+
+
 
 
 const Category = () => {
+    const [productos, setProductos] = useState([]);
+    const [url, setUrl] = useState('http://localhost:3001/api/products/list')
+    // traemos las categoras y el count
+    useEffect(() => {
+        fetch(url)
+        .then(res => res.json())
+        .then(productos =>{
+            console.log(productos)
+        })
+        .catch(e =>{
+            console.log(e)
+        })
+    },[ ])
+        
+     
+    
+    
+    
     return (
         <div className="col-lg-6 mb-4">
             <div className="card bg-info text-white shadow">
-				<div className="card-body">categoria..</div>
+				<div className="card-body">{productos}</div>
             </div>
 		</div>
     );
