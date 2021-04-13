357acf03af689024149887dd800f1e2ed104466d
lautaro porlan <l.porlann@gmail.com>
2021-04-12 21:24:59 -0300

    category component updated

--- src/components/Main/Category.js
+++ src/components/Main/Category.js
@@ -1,34 +1,15 @@
-import React , {useState , useEffect} from 'react';
+import React from "react";
 
-
-
-
-const Category = () => {
-    const [products, setProducts] = useState({});
-    const [url, setUrl] = useState('http://localhost:3001/api/products/list')
-    // traemos las categoras y el count
-    useEffect(() => {
-        fetch(url)
-        .then(res => res.json())
-        .then(products =>{
-           setProducts(products.meta.countByCategory)
-        })
-        .catch(e =>{
-            console.log(e)
-        })
-    },[ ])
-        
-     
-    
-    
-    
-    return (
-        <div className="col-lg-6 mb-4">
-            <div className="card bg-info text-white shadow">
-				<div className="card-body">.</div>
-            </div>
-		</div>
-    );
-}
+const Category = ({ nombre, cantidad }) => {
+  return (
+    <div className="col-lg-6 mb-4">
+      <div className="card bg-info text-white shadow">
+        <div className="card-body">
+          {nombre} - {cantidad}
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Category;
